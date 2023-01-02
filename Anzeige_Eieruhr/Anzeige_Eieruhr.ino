9ef43ec7befa6357f60d9c6e3d3ae93a827bb629
Franz <120483260+franzmhl@users.noreply.github.com>
2023-01-02 08:49:47 +0100

    Anzeige der Übergabewerte als Min/Sec

--- Anzeige_Eieruhr/Anzeige_Eieruhr.ino
+++ Anzeige_Eieruhr/Anzeige_Eieruhr.ino
@@ -37,7 +37,7 @@
 #define SET(x,y) (x |= y)
 #define CLEAR(x,y) ( x &=~ y)
 
-/* Namen fÃ¼r Bits */
+/* Namen für Bits */
 #define DATA  BIT(0)
 #define CLK   BIT(7)
 #define LATCH BIT(4)
@@ -52,13 +52,17 @@
 #define SEVEN 0xF8
 #define EIGHT 0x80
 #define NINE 0x90
+#define BLACK 0xFF
 
 #define DISP1 0xF1
 #define DISP3 0xF4
 #define DISP2 0xF2
 #define DISP4 0xF8
 
+unsigned char Muster1, Muster2, Muster3, Muster4;
 
+int wert_min = 3; //EIngabe der Testwerte für Minuten
+int wert_sec = 22; //Eingabe der Testwerte für Sekunden
 
 void Init_Ports() {
   SET(DDRB, DATA);              /* alle drei Bit Output */
@@ -192,60 +196,77 @@ void ANZ4(unsigned char Muster){
     CLEAR(PORTD, LATCH);
 }
 
+void UmwandlungZahlen(int wert_min, int wert_sec){
+  int wert_min1 = wert_min / 10;
+  int wert_min2 = wert_min % 10;
+  int wert_sec1 = wert_sec / 10;
+  int wert_sec2 = wert_sec % 10;
 
-void AnzeigeZiffer() {
- 
-  unsigned char Muster1 = ONE; //Anzeige 1 von links
-  unsigned char Muster2 = TWO; //Anzeige 2 von links
-  unsigned char Muster3 = THREE; //Anzeige 3 von links
-  unsigned char Muster4 = FOUR; //Anzeige 4 von links
-  /*unsigned char Stelle1 = DISP2;
-  unsigned char Stelle2 = DISP1;
-  unsigned char Stelle4 = DISP4;
-  unsigned char Stelle3 = DISP3;*/
-  
-
-	ANZ1(Muster1);
-  ANZ2(Muster2);
-  ANZ3(Muster3);
-  ANZ4(Muster4);
-
-
-  /*for (int i = 0; i < 8; i++) {
-      if (Muster1 & BIT(7)) {
-        SET(PORTB, DATA);
-      } else {
-        CLEAR(PORTB, DATA);
+  if ((wert_min1 && wert_min2) == 0){
+    Muster1=BLACK;
+    Muster2=BLACK;    
+  } 
+  else{
+      switch(wert_min1){
+        case 0: Muster1=ZERO; break;    
+        case 1: Muster1=ONE; break;
+        case 2: Muster1=TWO; break;
+  	    case 3: Muster1=THREE; break;
+        case 4: Muster1=FOUR; break;
+        case 5: Muster1=FIVE; break;
+        case 6: Muster1=SIX; break;
+  	    case 7: Muster1=SEVEN; break;
+        case 8: Muster1=EIGHT; break;
+        case 9: Muster1=NINE; break;
       }
-      Muster1 <<= 1;
-
-      
-      SET(PORTD, CLK);
-      CLEAR(PORTD, CLK);
-    }
-
-    
-    for (int i = 0; i < 8; i++) {
-      if (Stelle1 & BIT(7)) {
-        SET(PORTB, DATA);
-      } else {
-        CLEAR(PORTB, DATA);
+      switch(wert_min2){
+        case 0: Muster2=ZERO; break;    
+        case 1: Muster2=ONE; break;
+        case 2: Muster2=TWO; break;
+  	    case 3: Muster2=THREE; break;
+        case 4: Muster2=FOUR; break;
+        case 5: Muster2=FIVE; break;
+        case 6: Muster2=SIX; break;
+  	    case 7: Muster2=SEVEN; break;
+        case 8: Muster2=EIGHT; break;
+        case 9: Muster2=NINE; break;
       }
-      Stelle1 <<= 1;
-
-      SET(PORTD, CLK);
-      CLEAR(PORTD, CLK);
-    }*/
-
-
-  
-
+  }
+     switch(wert_sec1){
+        case 0: Muster3=ZERO; break;    
+        case 1: Muster3=ONE; break;
+        case 2: Muster3=TWO; break;
+  	    case 3: Muster3=THREE; break;
+        case 4: Muster3=FOUR; break;
+        case 5: Muster3=FIVE; break;
+        case 6: Muster3=SIX; break;
+  	    case 7: Muster3=SEVEN; break;
+        case 8: Muster3=EIGHT; break;
+        case 9: Muster3=NINE; break;
+      }
+  switch(wert_sec2){
+        case 0: Muster4=ZERO; break;    
+        case 1: Muster4=ONE; break;
+        case 2: Muster4=TWO; break;
+  	    case 3: Muster4=THREE; break;
+        case 4: Muster4=FOUR; break;
+        case 5: Muster4=FIVE; break;
+        case 6: Muster4=SIX; break;
+  	    case 7: Muster4=SEVEN; break;
+        case 8: Muster4=EIGHT; break;
+        case 9: Muster4=NINE; break;
+      }
 }
 
- 
+void AnzeigeZiffer() {
+UmwandlungZahlen(wert_min,wert_sec);
 
- 
-  
+ANZ1(Muster1);
+ANZ2(Muster2);
+ANZ3(Muster3);
+ANZ4(Muster4);
+}
+
 
 int main() {
   Init_Ports();
